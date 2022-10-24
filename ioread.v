4e68ab818cc9ff54bd26ee4a0e9d3edd39ed5449
zouyingcao <57442064+zouyingcao@users.noreply.github.com>
2022-10-24 23:51:07 +0800

    接口相关处理

--- ioread.v
+++ ioread.v
@@ -3,9 +3,13 @@
 
 module ioread (
     input			reset,					// 复位信号 
-    input			ior,					// 从控制器来的I/O读，
-    input			switchctrl,				// 从memorio经过地址高端线获得的拨码开关模块片选
+    input			ioRead,					// 从控制器来的I/O读，
+    input			switchCtrl,				// 从memorio经过地址高端线获得的拨码开关模块片选
     input	[15:0]	ioread_data_switch,		// 从外设来的读数据，此处来自拨码开关
+    input           keyboardCtrl,
+    input   [15:0]  ioread_data_keyboard,
+    input           timerCtrl,
+    input   [15:0]  ioread_data_timer,
     output	[15:0]	ioread_data				// 将外设来的数据送给memorio
 );
     
@@ -14,9 +18,13 @@ module ioread (
     always @* begin
         if(reset == 1)
             ioread_data = 16'b0000000000000000;
-        else if(ior == 1) begin
-            if(switchctrl == 1)
+        else if(ioRead == 1) begin
+            if(switchCtrl == 1)
                 ioread_data = ioread_data_switch;
+            if(keyboardCtrl)
+                ioread_data = ioread_data_keyboard;
+            if(timerCtrl)
+                ioread_data = ioread_data_timer;
             else   ioread_data = ioread_data;
         end
     end
--- memorio.v
+++ memorio.v
@@ -13,23 +13,36 @@ module memorio (
     output	[31:0]	rdata,			// data from memory or IO that want to read into register
     output	[31:0]	write_data,		// data to memory or I/O
     output	[31:0]	address,		// address to mAddress and I/O
-    output			LEDCtrl,		// LED CS
-    output			SwitchCtrl		// Switch CS
+    output          timerCtrl,      // 2个16位定时/计数器
+    output          keyboardCtrl,   // 4×4键盘控制器
+    output          digtalTubeCtrl, // 8位7段数码管
+    output          BuzzerCtrl,     // 蜂鸣管
+    output          WatchdogCtrl,   // 看门狗
+    output          PWMCtrl,        // PWM脉冲宽度调制
+    output			LEDCtrl,		// LED CS 
+    output			SwitchCtrl		// Switch CS 拨码开关
 );
    
 	reg[31:0] write_data;
-    wire iorw;
+    wire iorw;                     // 是否为IO操作
     
     assign  address = caddress;
-    assign  rdata = (memread==1) ? mread_data : {16'h00,ioread_data[15:0]};
+    // 若读取的数据来自IO，则进行零扩展
+    assign  rdata = (memread==1) ? mread_data : {16'h0000,ioread_data[15:0]};
     assign  iorw = (iowrite||ioread);
 	
-	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	assign	LEDCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC6)) ? 1'b1:1'b0;
-	assign	SwitchCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC7)) ? 1'b1:1'b0;
+	// 接口的地址
+	assign digtalTubeCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC0)) ? 1'b1:1'b0;
+	assign keyboardCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC1)) ? 1'b1:1'b0;
+	assign timerCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC2)) ? 1'b1:1'b0;
+	assign PWMCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC3)) ? 1'b1:1'b0;
+    assign WatchdogCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC5)) ? 1'b1:1'b0;
+    assign LEDCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC6)) ? 1'b1:1'b0;
+    assign SwitchCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFC7)) ? 1'b1:1'b0;
+	assign BuzzerCtrl = ((iorw==1) && (caddress[31:4] == 28'hFFFFFD1)) ? 1'b1:1'b0;
 							
-    always @* begin
-        if((memwrite==1)||(iowrite==1)) begin
+    always @(*) begin
+        if(memwrite||iowrite) begin
             write_data = wdata;
         end else begin
             write_data = 32'hZZZZZZZZ;
